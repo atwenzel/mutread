e65bd758fbae7f93044502ba63e2e6f3818bb634
atwenzel <alexanderwenzel2017@u.northwestern.edu>
2015-12-18 10:44:51 -0600

    first

---
+++ driver.py
@@ -0,0 +1,11 @@
+"""Welcome to MutRead.  The goal for all this software is for you to be able to put a vcf files in the 'data' folder
+and do anything you can imagine with them.
+
+Questions/concerns: Alex Wenzel (alexanderwenzel2017@u.northwestern.edu)"""
+
+#Global
+
+#Local
+
+if __name__ == "__main__":
+    print("welcome to MutRead")
